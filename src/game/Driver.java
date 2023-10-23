6d5811199a2891c21b1e15008604210561f0adcf
Eric <eric.ucl@outlook.com>
2023-10-23 03:29:47 -0700

    v1.1 for Gradescope

--- src/game/Driver.java
+++ src/game/Driver.java
@@ -22,12 +22,12 @@ public class Driver {
   public static void main(String[] args) {
     try {
       // read file path from args
-            if (args.length < 1) {
-              throw new IllegalArgumentException("Please provide a file path as an argument.");
-            }
-//      args = new String[2];
-//      args[0] = "res/mansion.txt";
-//      args[1] = "10";
+      if (args.length < 1) {
+        throw new IllegalArgumentException("Please provide a file path as an argument.");
+      }
+      //      args = new String[2];
+      //      args[0] = "res/mansion.txt";
+      //      args[1] = "10";
       int maxTurns = Integer.parseInt(args[1]);
       Readable fileReader = new InputStreamReader(new FileInputStream(args[0]));
       final Readable in = new InputStreamReader(System.in);
