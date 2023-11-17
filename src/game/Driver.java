bad55619aad8edf7e916d1dbbf3881bc595fae42
Eric <eric.ucl@outlook.com>
2023-11-17 02:30:45 -0800

    add evidence function

--- src/game/Driver.java
+++ src/game/Driver.java
@@ -25,9 +25,6 @@ public class Driver {
       if (args.length < 1) {
         throw new IllegalArgumentException("Please provide a file path as an argument.");
       }
-      //      args = new String[2];
-      //      args[0] = "res/mansion.txt";
-      //      args[1] = "10";
       int maxTurns = Integer.parseInt(args[1]);
       Readable fileReader = new InputStreamReader(new FileInputStream(args[0]));
       final Readable in = new InputStreamReader(System.in);
